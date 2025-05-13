371c4e4acdee4c0573e4e267aa24468225738d83
Brian Wardwell <briwar10@outlook.com>
2025-05-13 15:54:24 -0500

    adding hero module imports

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,1 +1,8 @@
 // main TSX file
+ 
+// hero imports
+import InstagramLineIcon from 'remixicon-react/InstagramLineIcon'
+import FacebookCircleLineIcon from 'remixicon-react/FacebookCircleLineIcon'
+import DribbbleLineIcon from 'remixicon-react/DribbbleLineIcon'
+import YoutubeLineIcon from 'remixicon-react/YoutubeLineIcon'
+import GithubLineIcon from 'remixicon-react/GithubLineIcon'
