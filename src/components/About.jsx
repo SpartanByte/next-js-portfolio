46637b4080e074f58c864cd9afb7f36bd9933c3f
Brian Wardwell <briwar10@outlook.com>
2025-06-25 22:06:24 -0500

    create About component

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Heading from './sub/Heading'
+import Image from 'next/image'
+import { aboutData, aboutText, downloadIcon, arrowLeftIcon } from '@/assets'
+
+const About = () => {
+
+    return(
+        <div id="about" className="min-h-screen flex flex-col items-center justify-center">
+            {/* Heading component will likely go here */}
+            {/* Followed by portfolio picture of me */}
+        </div>
+    );
+}
+
+export default About
