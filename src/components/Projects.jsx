2c281a8d093f583cbc4a946d200339905b935d3b
Brian Wardwell <briwar10@outlook.com>
2025-07-02 19:47:12 -0500

    add Projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState, useRef, useEffect } from 'react'
+import { portfolioProjectData } from '../assets'
+import { animate, motion } from 'framer-motion'
+
+import SectionHeading from './sub/SectionHeading'
+import Project from './sub/Project'
+
+const Projects = () => {
+
+}
+
+export default Projects
