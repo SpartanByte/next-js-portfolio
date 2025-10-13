f909df2960ed9aa6c81693a8482b251bcd41e5b2
Brian Wardwell <briwar10@outlook.com>
2025-10-12 21:36:02 -0500

    update alt values for skills logos

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -35,11 +35,11 @@ const variants = {
                         className="flex items-center justify-center gap-x-3 rounded-xl border  bg-zinc-200 px-5 py-2 lg:px-2"
                     >
                         <Image
-                        src={item.icon}
-                        alt="Skills Image"
-                        width={100}
-                        height={100}
-                        className="h-auto w-[40px]"
+                            src={item.icon}
+                            alt={item.name + 'logo image' || 'Skill Logo'}
+                            width={100}
+                            height={100}
+                            className="h-auto w-[40px]"
                         />
                         <p className="text-sm text-gray-600 font-bold">{item.name}</p>
                     </motion.div>
@@ -59,11 +59,11 @@ const variants = {
                         className="flex items-center justify-center gap-x-3 rounded-xl border bg-zinc-200 px-5 py-2 lg:px-2"
                     >
                         <Image
-                        src={item.icon}
-                        alt="Skills Image"
-                        width={100}
-                        height={100}
-                        className="h-auto w-[40px]"
+                            src={item.icon}
+                            alt={item.name + 'logo image' || 'Skill Logo'}
+                            width={100}
+                            height={100}
+                            className="h-auto w-[40px]"
                         />
                         <p className="text-sm text-gray-600 font-bold">{item.name}</p>
                     </motion.div>
@@ -83,11 +83,11 @@ const variants = {
                         className="flex items-center justify-center gap-x-3 rounded-xl border bg-zinc-200 px-5 py-2 lg:px-2"
                     >
                         <Image
-                        src={item.icon}
-                        alt="Skills Image"
-                        width={100}
-                        height={100}
-                        className="h-auto w-[40px]"
+                            src={item.icon}
+                            alt={item.name + 'logo image' || 'Skill Logo'}
+                            width={100}
+                            height={100}
+                            className="h-auto w-[40px]"
                         />
                         <p className="text-sm text-gray-600 font-bold">{item.name}</p>
                     </motion.div>
